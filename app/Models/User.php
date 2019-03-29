c58f1592b7aa6503a1d927bfca724f31dd89f29b
lixiang.han <lixiang.han@compasslist.com>
2019-03-29 09:29:55 +0000

    move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,9 +9,12 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+//    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
+     * 只有 $fillable 包含的字段才能被批量赋值
      * @var array
      */
     protected $fillable = [
@@ -21,6 +24,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for arrays.
      *
+     * 隐藏敏感信息
      * @var array
      */
     protected $hidden = [
