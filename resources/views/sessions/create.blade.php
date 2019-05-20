a3909c2eadedb08fc03948d89790a0f948c4ab38
phphlx <phphlx@163.com>
2019-05-20 11:58:36 +0000

    add password.reset

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -19,7 +19,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="password">密码:</label>
+                        <label for="password">密码 (<a href="{{ route('password.request') }}">忘记密码</a>) :</label>
                         <input type="password" value="{{ old('password') }}" class="form-control" name="password">
                     </div>
 
