27198f011c36a95b0c1f737afaf744abd5547bd5
phphlx <phphlx@163.com>
2019-07-19 13:25:49 +0000

    add user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +10,29 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        session()->flash('success', 'welcome, a free try');
+
+        return redirect()->route('users.show', $user);
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.default')
 @section('title', 'register')
 @section('content')
-    <h1>register</h1>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h5>register</h5>
+            </div>
+            <div class="panel-body">
+                @include('shared._errors')
+
+                <form action="{{ route('users.store') }}" method="POST">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="name">name:</label>
+                        <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">email:</label>
+                        <input type="email" class="form-control" value="{{ old('email') }}" name="email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">password:</label>
+                        <input type="password" name="password" value="{{ old('password') }}" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">repassword:</label>
+                        <input type="password" name="password_confirmation" value="{{ old('password_confirmation')
+                        }}" class="form-control">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">register</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
 @endsection
