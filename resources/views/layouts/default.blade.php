a5442287f482cf6efb4c2ca89e61dba5e8b4b01c
phphlx <phphlx@163.com>
2020-08-18 12:06:52 +0000

    样式初始化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,20 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'Weibo App') - Laravel 新手入门</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a href="/" class="navbar-brand">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+  @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.default')
 
 @section('content')
-  <h1>home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      现在所看到的是 <a href="#">Laravel 入门教程</a> 的示例工程
+    </p>
+    <p>一切从这里开始</p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success">注册</a>
+    </p>
+  </div>
 @endsection
