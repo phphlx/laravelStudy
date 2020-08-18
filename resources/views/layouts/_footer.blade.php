04ae02d362c2a11a482edfad0562dc5f2e2702c8
phphlx <phphlx@163.com>
2020-08-18 12:30:34 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
+  <a href="#">刻意练习, 每日精进</a>
+
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
