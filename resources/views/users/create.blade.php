4c5ff7ecebd5861dfaa4970017b32f2f5d7efa3d
phphlx <phphlx@163.com>
2020-08-19 06:57:24 +0000

    user register form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', '注册')
 
 @section('content')
-  <h1>注册</h1>
+  <div class="offset-md-2 col-md-8">
+    <div class="card">
+      <div class="card-header">
+        <h5>register</h5>
+      </div>
+      <div class="card-body">
+        <form action="{{ route('users.store') }}" method="POST">
+          <div class="form-group">
+            <label for="name">name:</label>
+            <input type="text" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">email:</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">password:</label>
+            <input type="password" name="password" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">password_confirm</label>
+            <input type="password" name="password_confirmation" class="form-control">
+          </div>
+
+          <button class="btn btn-primary">register</button>
+        </form>
+      </div>
+    </div>
+  </div>
 @endsection
