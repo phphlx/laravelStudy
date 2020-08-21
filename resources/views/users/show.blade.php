05a4da4c69776d46705c2f9d6d8c861cd8f4302d
phphlx <phphlx@163.com>
2020-08-21 03:06:14 +0000

    add weibo list

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,15 +2,26 @@
 @section('title', $user->name)
 
 @section('content')
-<div class="row">
-  <div class="col-md-8 offset-md-2">
-    <div class="col-md-12">
-      <div class="col-md-8 offset-md-2">
-        <section class="user_info">
-          @include('shared._user_info', ['user' => $user])
-        </section>
-      </div>
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <section class="user_info">
+        @include('shared._user_info', ['user' => $user])
+      </section>
+
+      <section class="status">
+        @if ($statuses->count() > 0)
+          <ul class="list-unstyled">
+            @foreach($statuses as $status)
+              @include('statuses._status')
+            @endforeach
+          </ul>
+          <div class="mt-5">
+            {!! $statuses->render() !!}
+          </div>
+        @else
+          <p>没有数据 !</p>
+        @endif
+      </section>
     </div>
   </div>
-</div>
 @endsection
