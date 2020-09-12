5e16d5904ed8ef464827220954922fb7406c90e6
phphlx <phphlx@163.com>
2020-09-12 03:12:45 +0000

    init style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,20 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'Weibo') - Laravel 新手入门</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+  <div class="container">
+    <a href="/" class="navbar-brand">Home</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+  @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,14 @@
 @section('title', 'home')
 
 @section('content')
-  <h1>Home</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页.
+    </p>
+    <p>一切, 从这里开始.</p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+    </p>
+  </div>
 @endsection
