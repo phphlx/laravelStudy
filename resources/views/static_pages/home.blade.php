31392724205a326f9e0727e2f66ece871057e812
phphlx <phphlx@163.com>
2020-09-18 09:30:38 +0000

    register page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,7 +9,7 @@
     </p>
     <p>一切, 从这里开始.</p>
     <p>
-      <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+      <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
     </p>
   </div>
 @endsection
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'register')
+
+@section('content')
+  <h1>register</h1>
+@endsection
