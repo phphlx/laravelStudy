3368c039b312ef921b09c9521a67584566928d41
phphlx <phphlx@163.com>
2020-09-20 13:23:27 +0000

    user create form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'register')
 
 @section('content')
-  <h1>register</h1>
+  <div class="offset-md-2 col-md-8">
+    <div class="card">
+      <div class="card-header">
+        <h5>register</h5>
+      </div>
+      <div class="card-body">
+        <form action="{{ route('users.store') }}" method="POST">
+          <div class="form-group">
+            <label for="name">name:</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">email:</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">password:</label>
+            <input type="password" class="form-control" name="password">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">repassword:</label>
+            <input type="password" name="password_confirmation" class="form-control">
+          </div>
+
+          <button class="btn btn-primary">register</button>
+        </form>
+      </div>
+    </div>
+  </div>
 @endsection
